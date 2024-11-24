0cdf08fb02465d00bb75bf6bd97a8e69ecac3557
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2024-11-24 14:18:17 +0900

    feat: TokenInfo 요청 @RequestParm 으로 수정

--- src/main/java/com/eventorauth/auth/client/UserInfoClient.java
+++ src/main/java/com/eventorauth/auth/client/UserInfoClient.java
@@ -2,12 +2,12 @@ package com.eventorauth.auth.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.eventorauth.auth.dto.response.GetUserTokenInfoResponse;
 
 @FeignClient(name = "UserInfoService", url = "http://localhost:8083")
 public interface UserInfoClient {
 	@GetMapping("/back/users/info")
-	GetUserTokenInfoResponse getUserTokenInfoByIdentifier(@RequestHeader("X-User-userId") String identifier);
+	GetUserTokenInfoResponse getUserTokenInfoByIdentifier(@RequestParam String identifier);
 }
